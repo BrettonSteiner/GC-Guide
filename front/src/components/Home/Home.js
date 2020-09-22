8db7bfa2eeeb8ee0f9de08ff9065532850bf3d7d
SpyRock28 <imbradensteiner@gmail.com>
2020-09-21 18:31:25 -0600

    refactored structure for tests

--- front/src/components/Home/Home.js
+++ front/src/components/Home/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import StudentContextProvider from '../contexts/StudentContext';
-import Email from './Email';
-import ITeam from './ITeam';
-import MajorCollege from './MajorCollege';
-import Schedule from './Schedule';
+import StudentContextProvider from '../../contexts/StudentContext/StudentContext';
+import Email from '../Email/Email';
+import ITeam from '../ITeam/ITeam';
+import MajorCollege from '../MajorCollege/MajorCollege';
+import Schedule from '../Schedule/Schedule';
 
 const Home = (props) => {
   return(
