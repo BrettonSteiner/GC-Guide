72f140d31a988c4c7f51641e46690a0b9000ec0f
SpyRock28 <imbradensteiner@gmail.com>
2020-09-24 13:46:31 -0600

    added first test and switched to enzyme and jest for testing

---
+++ front/src/components/Home/Home.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Home from './Home.js';
+
+describe('Home Component Tests', () => {
+  it('Should test the header stuff', () => {
+    
+  });
+})
