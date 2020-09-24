8c65d0dc0e5ce71cf7a96449b11928c7480f36a5
SpyRock28 <imbradensteiner@gmail.com>
2020-09-24 13:51:39 -0600

    tests added

--- front/src/components/Home/Home.test.js
+++ front/src/components/Home/Home.test.js
@@ -3,7 +3,16 @@ import { shallow } from 'enzyme';
 import Home from './Home.js';
 
 describe('Home Component Tests', () => {
-  it('Should test the header stuff', () => {
-    
+  describe('Header Tests', () => {
+    it('Should test the header stuff', () => {
+      const wrapper = shallow(<Home/>);
+      expect(wrapper.find('#header').length).toEqual(1);
+    });
+  })
+  describe('Footer tests', () => {
+    it('Should test footer stuff', () => {
+      const wrapper = shallow(<Home/>);
+      expect(wrapper.find('#footer').length).toEqual(1);
+    });
   });
 })
