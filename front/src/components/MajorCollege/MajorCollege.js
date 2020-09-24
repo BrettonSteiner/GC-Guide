8cb7dc949a570344fdfc24db3fa28886a838a41f
BrettonSteiner <imbrettonsteiner@gmail.com>
2020-09-24 15:26:53 -0600

    Moved all accordion logic to the Home component.

--- front/src/components/MajorCollege/MajorCollege.js
+++ front/src/components/MajorCollege/MajorCollege.js
@@ -7,23 +7,12 @@ const MajorCollege = (props) => {
   //   //Call database for data
   // }, [])
   return (<>
-  <div class="card">
-    <button class="btn btn-light btn-block text-left card-header" id="headingTwo" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-      <h5 class="mb-0">
-        My Academic Connections College
-      </h5>
-    </button>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        <form>
-          <div class="form-group">
-            <label for="majorName">Major Name</label>
-            <input type="text" class="form-control" id="majorName" placeholder="Major Name"></input>
-          </div>
-        </form>
-      </div>
+  <form>
+    <div class="form-group">
+      <label for="majorName">Major Name</label>
+      <input type="text" class="form-control" id="majorName" placeholder="Major Name"></input>
     </div>
-  </div>
+  </form>
   </>);
 }
 
--- front/src/components/Schedule/Schedule.js
+++ front/src/components/Schedule/Schedule.js
@@ -5,18 +5,7 @@ const Schedule = (props) => {
   //   //Call database for data
   // }, [])
   return (<>
-  <div class="card">
-    <button class="btn btn-light btn-block text-left card-header" id="headingThree" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-      <h5 class="mb-0">
-        Get Connected Schedule
-      </h5>
-    </button>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        Schedule content
-      </div>
-    </div>
-  </div>
+  Schedule content
   </>);
 }
 
