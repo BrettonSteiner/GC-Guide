e3e5db05219e20b3b4ca914518f6b6112e606231
SpyRock28 <imbradensteiner@gmail.com>
2020-10-02 15:17:41 -0600

    made the checkboxes work with useState

--- front/src/components/Email/Email.js
+++ front/src/components/Email/Email.js
@@ -3,6 +3,8 @@ import React, {useState,/*useContext*/} from 'react';
 
 const Email = (props) => {
   // const {ITeamNumber, major, college, setITeamError, setCollegeError} = useContext(StudentContext);
+  const [includeITeam, setIncludeITeam] = useState(false);
+  const [includeAcInfo, setIncludeAcInfo] = useState(false);
   const [includeSchedule, setIncludeSchedule] = useState(true);
   return (<>
   <div className="form-group">
@@ -11,19 +13,19 @@ const Email = (props) => {
     <small id="emailHelp" className="form-text text-muted">This will not subscribe you to anything. It is a one-time email only.</small>
   </div>
   <div className="form-check">
-    <input className="form-check-input" type="checkbox" value="" id="iTeamCheck"></input>
+    <input className="form-check-input" type="checkbox" value={includeITeam} id="iTeamCheck" onChange={(e) => setIncludeITeam(e.target.checked)}></input>
     <label className="form-check-label" htmlFor="iTeamCheck">
       Include I-Team information
     </label>
   </div>
   <div className="form-check">
-    <input className="form-check-input" type="checkbox" value="" id="collegeCheck"></input>
+    <input className="form-check-input" type="checkbox" value={includeAcInfo} id="collegeCheck" onChange={(e) => setIncludeAcInfo(e.target.checked)}></input>
     <label className="form-check-label" htmlFor="collegeCheck">
       Include Academic Connections information
     </label>
   </div>
   <div className="form-group form-check">
-    <input className="form-check-input" type="checkbox" checked={includeSchedule} id="scheduleCheck" onClick={(value) => setIncludeSchedule(value)}></input>
+    <input className="form-check-input" type="checkbox" value={includeSchedule} id="scheduleCheck" onChange={(event) => setIncludeSchedule(event.target.checked)} defaultChecked></input>
     <label className="form-check-label" htmlFor="scheduleCheck">
       Include Get Connected schedule
     </label>
