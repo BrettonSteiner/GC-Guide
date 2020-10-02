7cd362da3a380fcc70ace946db61fb17159331b3
SpyRock28 <imbradensteiner@gmail.com>
2020-10-02 16:51:10 -0600

    emails first flesh out

--- front/src/components/Email/Email.js
+++ front/src/components/Email/Email.js
@@ -1,15 +1,51 @@
-import React, {useState,/*useContext*/} from 'react';
-// import { StudentContext } from '../contexts/StudentContext';
+import React, {useState, useContext, useCallback} from 'react';
+// import { StudentContext } from '../../contexts/StudentContext.js';
+import { StudentContext } from '../../contexts/StudentContext/StudentContext.js';
 
 const Email = (props) => {
-  // const {ITeamNumber, major, college, setITeamError, setCollegeError} = useContext(StudentContext);
+  const [email, setEmail] = useState("");
   const [includeITeam, setIncludeITeam] = useState(false);
   const [includeAcInfo, setIncludeAcInfo] = useState(false);
   const [includeSchedule, setIncludeSchedule] = useState(true);
+  const [emailError, setEmailError] = useState(false);
+  const [errorsExist, setErrorsExist] = useState(false);
+
+  const {ITeamNumber, major, college, ITeamError, setITeamError, collegeError, setCollegeError} = useContext(StudentContext);
+
+  const checkForErrors = useCallback(() => {
+    let hasErrors = false;
+
+    if (includeITeam && !ITeamNumber) {
+      setITeamError(true);
+      hasErrors = true;
+    }
+
+    if (includeAcInfo && (!major || !college)) {
+      setCollegeError(true);
+      hasErrors = true;
+    }
+
+    if (!email || !email.includes("@byui.edu")) {
+      setEmailError(true);
+      hasErrors = true;
+    } 
+
+    setErrorsExist(hasErrors);
+
+    return hasErrors;
+  }, [includeITeam, ITeamNumber, includeAcInfo, major, college, email, setCollegeError, setITeamError]);
+
+  const sendEmail = () => {
+    let hasErrors = checkForErrors();
+    if (!hasErrors) {
+      //send email
+    }
+  }
+
   return (<>
   <div className="form-group">
     <label htmlFor="byuiEmail">BYU-I Email Address</label>
-    <input type="email" className="form-control" id="byuiEmail" aria-describedby="emailHelp" placeholder="BYU-I Email Address"></input>
+    <input type="email" className="form-control" id="byuiEmail" aria-describedby="emailHelp" placeholder="BYU-I Email Address" value={email} onChange={(e) => setEmail(e.target.value)}/>
     <small id="emailHelp" className="form-text text-muted">This will not subscribe you to anything. It is a one-time email only.</small>
   </div>
   <div className="form-check">
@@ -30,9 +66,20 @@ const Email = (props) => {
       Include Get Connected schedule
     </label>
   </div>
-  <button type="button" className="btn btn-primary">Send Email</button>
+  <button type="button" className="btn btn-primary" onClick={sendEmail}>Send Email</button>
   <div id="emailResult">
     <br></br>
+    {errorsExist ? (<>
+      <div className="alert alert-danger">
+      {`Error: `} 
+        {emailError ? ` Invalid email address. Must be '...@byui.edu'.` : null}
+        {ITeamError ? ` Missing I-Team Information.` : null}
+        {collegeError ? ` Missing Academic Connections information.` : null}
+      <button type="button" className="close" aria-label="Close" onClick={() => setErrorsExist(false)}>
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    </>) : null}
     <div className="alert alert-success" role="alert">
       Email sent.
       <button type="button" className="close" data-dismiss="alert" aria-label="Close">
