eafd40f94eee074e32521552f5142d99e1149069
BrettonSteiner <imbrettonsteiner@gmail.com>
2020-10-20 16:16:08 -0600

    First attempt at mongodb and docker
    This currently isn't working for me, so more work is needed.

--- back/routes/userRouter.js
+++ back/routes/userRouter.js
@@ -5,4 +5,7 @@ var userService = require('../services/userService');
 /* GET users listing. */
 router.get('/', userService.getUsers);
 
+/* GET create user test. */
+router.get('/create', userService.createUser);
+
 module.exports = router;
