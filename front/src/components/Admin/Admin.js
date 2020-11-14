4a0765c89fe63a0c92cb025cc8cdb2ddf18f7039
SpyRock28 <imbradensteiner@gmail.com>
2020-11-14 10:27:56 -0700

    first go at admin

---
+++ front/src/components/Admin/Admin.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Admin.css';
+
+const Admin = (props) => {
+  return(
+    <>
+      <div className="header" id="header">
+        <div className="container" >
+          <div className="header-logo"></div>
+          <h4 className="header-text">GC Guide Admin</h4>
+        </div>
+      </div>
+      {/* Put admin stuff here */}
+    </>
+  );
+}
+
+export default Admin;
