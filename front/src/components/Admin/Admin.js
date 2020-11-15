f53b90c91d78cec9d48fd751dd2b31fd09b1d01c
SpyRock28 <imbradensteiner@gmail.com>
2020-11-14 19:38:36 -0700

    semester selection and activation

--- front/src/components/Admin/Admin.js
+++ front/src/components/Admin/Admin.js
@@ -2,8 +2,42 @@ import React, {useState, } from 'react';
 import Tabs from '../Tabs/Tabs.js';
 import './Admin.css';
 
+const testSemesters = [
+  {name: "Winter 2020", activeFlag: false},
+  {name: "Spring 2020", activeFlag: false},
+  {name: "Fall 2020", activeFlag: true},
+  {name: "Winter 2021", activeFlag: false},
+]
+
 const Admin = (props) => {
-  const [selectedSemester, setSelectedSemester] = useState({});
+  const [semesters, setSemesters] = useState(testSemesters);
+  const [selectedSemesterName, setSelectedSemesterName] = useState(semesters[0]?.name);
+  let selectedSemester = semesters.find(sem => sem.name === selectedSemesterName);
+
+  let changeSemester = (e) => {
+    setSelectedSemesterName(e.target.value);
+  };
+
+  let activateSemester = () => {
+    setSemesters(currSemesters => {
+      return currSemesters.map(sem => {
+        if (sem.name === selectedSemesterName)
+          return {...sem, activeFlag: true}
+        return {...sem, activeFlag: false}
+      });
+    });
+  };
+
+  let deactivateSemester = () => {
+    setSemesters(currSemesters => {
+      return currSemesters.map(sem => {
+        if (sem.name === selectedSemesterName)
+          return {...sem, activeFlag: false}
+        return {...sem}
+      });
+    });
+  };
+
   return(
     <>
       <div className="header" id="header">
@@ -13,7 +47,17 @@ const Admin = (props) => {
         </div>
       </div>
       <div className="container">
-        {/* Put admin stuff here */}
+        <div className="semester-div">
+          <select className="form-control semester-select" id="semester-select" value={selectedSemester?.name}
+            onChange={changeSemester}>
+              {semesters.map(semester => {
+                return (<option key={semester.name} value={semester.name}>{semester.name}{semester.activeFlag ? " (Active)" : ""}</option>)
+              })}
+          </select>
+          {selectedSemester?.activeFlag
+          ? (<input type="button" value="Deactivate Semester" className="btn btn-warning" onClick={deactivateSemester}/>)
+          : (<input type="button" value="Set As Active Semester" className="btn btn-info" onClick={activateSemester}/>)}
+        </div>
         <Tabs>
           <div label="I-Team">
             <h5>I-Team Stuff</h5>
