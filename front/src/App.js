61150ccdd510c0ae5c0a120b7ce2418a3b2bc2c8
SpyRock28 <imbradensteiner@gmail.com>
2020-11-17 17:46:59 -0700

    added router but haven't locked down admin

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Admin from './components/Admin/Admin';
 import Home from './components/Home/Home.js';
@@ -7,8 +7,12 @@ import Home from './components/Home/Home.js';
 function App() {
   return (
     <div className="App">
-     {/* <Home /> */}
-     <Admin />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/admin" component={Admin} /> 
+        </Switch>
+     </Router>
     </div>
   );
 }
